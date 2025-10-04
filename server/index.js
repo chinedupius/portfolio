b6cb541ae9fca08832448b501a794842587c9ee8
chinedupius <ukachukwuchinedupius@yahoo.com>
2025-10-04 15:43:48 +1000

    Initial portfolio publish

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.post('/contact', express.urlencoded({ extended: true }), (req, res) => {
+  console.log('Contact Form Submission:', req.body);
+  res.send('Message received. Thank you!');
+});
+
+app.get('/download/:filename', (req, res) => {
+  const file = path.join(__dirname, 'evidence', req.params.filename);
+  res.download(file);
+});
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
